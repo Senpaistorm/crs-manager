48a5f7a3f4775ebd353616fed94b9009356007a8
Senpaistorm <austin.l@qq.com>
2019-01-15 15:25:12 -0500

    Added admin support for changing grade

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Course, Assessment
+from .models import Course, Assessment,Grade
 
 admin.site.register(Course)
 
 admin.site.register(Assessment)
+
+admin.site.register(Grade)
